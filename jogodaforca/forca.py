1cf93dea16d07c888af3e2c5be927d097264640b
Vinicius Salvador <viniciussalvador05@gmail.com>
2022-07-17 22:44:49 -0300

    ajustando funcoes

--- jogodaforca/forca.py
+++ jogodaforca/forca.py
@@ -1,88 +1,147 @@
-#Imports
 import random
-from pathlib import Path
-
-#Variaveis
-nome_arquivo = ''
-
-
-def perguntar_letra():
-    letra = input('Digite a letra: ')
-    return letra
-
-
 
 def jogar():
-
     imprimir_menu()
-
     tema = escolher_tema()
-    nome_arquivo = pegar_nome_arquivo(tema)
-    palavra_secreta = pegar_palavra_secreta(nome_arquivo)
 
-    letras_acertadas = ['_' for letras in palavra_secreta]
+    palavra_secreta = carregar_palavra(tema)
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     erros = 0
-    venceu = False
+    letra_chutes = ''
+
+    ganhou = False
     perdeu = False
-    print(palavra_secreta)
-    print(type(palavra_secreta))
-    while (not venceu and not perdeu):
-        chute = input('Informe a letra: ')
-        for letras in palavra_secreta:
-            if chute == palavra_secreta[letras]:
-                letras_acertadas[letras] == chute
-            else:
-                erros += 1
-        perdeu = (erros == 6)
 
+    while (not ganhou and not perdeu):
+        imprimir_forca(erros)
+        print(letras_acertadas)
+        print(f'Palavras que já foram: {letra_chutes}')
 
+        chute = validar_chute('Digite uma letra: ', letra_chutes)
+        letra_chutes += chute
 
-    print(palavra_secreta)
-    print(letras_acertadas)
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-def pegar_linhas(nome_arquivo: str):
-    path = './jogodaforca/palavras'
-    arquivo = Path(path).joinpath(nome_arquivo)
-    if not arquivo.is_file():
-        raise ValueError(f'Arquivo {arquivo} não existe')
+        perdeu = erros == 6
+        ganhou = '_' not in letras_acertadas
 
-    with open(arquivo, "r", encoding="utf-8") as fp:
-        return [line.strip('\n') for line in fp.readlines()]
+    if ganhou:
+        print('Parabens, você venceu!')
+    else:
+        print('Puxa, você foi enforcado!')
+        print(f'A palavra era {palavra_secreta}')
 
-def pegar_palavra_secreta(nome):
-    palavras = pegar_linhas(nome)
-    numero_palavra = random.randrange(0, len(palavras))
-    return palavras[numero_palavra]
+def validar_chute(msg, letra_chutes):
+    chute = input(msg)
+    while (not chute.isalpha()) or (chute in letra_chutes):
+        chute = input('Chute invalido, tente novamente:')
+    print('=-=' * 10)
+    return chute
 
-def pegar_nome_arquivo(tema):
+def carregar_palavra(tema):
+    palavras = []
+    nome_arquivo = pegar_arquivo(tema)
+    arquivo = open(nome_arquivo, 'r', encoding="utf-8")
+
+    for linha in arquivo:
+        linha = linha.strip('\n')
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    return palavras[numero].upper()
+
+def pegar_arquivo(tema):
+    if __name__ == '__main__':
+        path = './palavras/'
+    else:
+        path = './jogodaforca/palavras/'
+
     match tema:
         case '1':
-            nome_arquivo = 'animais.txt'
+            nome_arquivo = (path + 'animais.txt')
         case '2':
-            nome_arquivo = 'frutas.txt'
+            nome_arquivo = (path + 'frutas.txt')
         case '2':
-            nome_arquivo = 'objetos.txt'
+            nome_arquivo = (path + 'objetos.txt')
     return nome_arquivo
-
-def imprimir_temas():
-    print('TEMAS DISPONIVEIS ')
-    print('','[1] Animais','[2] Frutas','[3] Objetos', '', sep='\n')
 
 def escolher_tema():
     imprimir_temas()
     tema = input('Escolha um dos temas: ')
     while tema not in '123':
         tema = input('Informe um tema válido: ')
+    print('=-=' * 10)
     return tema
+
+def imprimir_temas():
+    print('TEMAS DISPONIVEIS ')
+    print('','[1] Animais','[2] Frutas','[3] Objetos', '', sep='\n')
 
 def imprimir_menu():
     print('=-=' * 10)
     print('      ◆ JOGO DA FORCA ◆')
     print('=-=' * 10)
 
-if __name__ == '__main__':
+
+def imprimir_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |       |    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+if (__name__ == "__main__"):
     jogar()
-    a = input('digite uma letra')
-
-
+    a = input('a')
